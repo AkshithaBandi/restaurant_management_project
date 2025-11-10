3dfa44873f86b62e82349d502ce9e23d190fb36d
AkshithaBandi <akshithabandi29@gmail.com>
2025-11-10 20:39:43 +0530

    60216

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+status = models.ForeignKey(OrderStatus,on_delete = models.SET_NULL, null = True)
+
+#in terminal
+python manage.py makemigrations orders
+python manage.py migrate
