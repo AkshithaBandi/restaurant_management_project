8041ce0a14c6d4172963501f6c9b4ba47588996c
AkshithaBandi <akshithabandi29@gmail.com>
2025-11-10 20:42:59 +0530

    60217

--- home/models.py
+++ home/models.py
@@ -7,6 +7,14 @@ class MenuCategory(models.Model):
     def __str__(self):
         return self.name
 
+from django.db import models
+
+class OrderStatus(models.Model):
+    name = models.CharField(max_length =  50, unique = True)
+
+    def __str__(self):
+        return self.name
+
 #in terminal
 
 python manage.py makemigrations home
